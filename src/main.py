1530a1fd5030f7bc7e533937161cb6101186d196
Dwight Spencer <dspencer@computekindustries.com>
2018-03-18 04:47:40 -0500

    Update main.py
    Cleaned up code for readability and included PEP compliant lines.
    Made python auto close

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import BrainfuckMachine 
 
-machine = BrainfuckMachine.BrainfuckMachine(50)
-f = open('helloworld.bf', 'r')
-machine.setCode(f.read())
-f.close()
-machine.run()
-print machine.getOutput()
+def main():
+
+  machine = BrainfuckMachine.BrainfuckMachine(50)
+  with open('helloworld.bf', 'r') as f:
+    machine.setCode(f.read())
+
+  machine.run()
+  print machine.getOutput()
+
+if __name__ is "__main__": main()
